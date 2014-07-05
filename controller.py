5385e9f5798f2d4f498542f748f7a4d5b14aa6bf
MROS <yc1043@gmail.com>
2014-07-05 07:48:26 +0000

    set unique motor, and descend slowly

--- controller.py
+++ controller.py
@@ -18,7 +18,7 @@ class quadcopter(object):
 		print('***Disconnect ESC power')
 		print('***then press ENTER')
 		res = raw_input()
-		for (name, mo) in self.motors:
+		for (name, mo) in self.motors.values():
 			mo.start()
 			mo.setW(100)
 		#NOTE:the angular motor speed W can vary from 0 (min) to 100 (max)
@@ -29,9 +29,27 @@ class quadcopter(object):
 		res = raw_input()
 		mymotor.setW(0)
 
+	def set_all_to(self, s):
+		for mo in self.motors.values():
+			mo.set_speed(s)
+	def set_unique_to(self, mo, s):
+		self.motors[mo].set_speed(s)
+
 	def take_off(self):
-		for (name, mo) in self.motors:
-			mo.set_speed(50)
+		self.set_all_to(50)
+	
+	def balance(self):
+		# TODO: make quadcopter balance(stop in the air), wait for the study of IMU
+		pass
+
+	def descend(self):
+		# TODO: It should have different behavior in different height and speed, wait for the study of IMU
+		
+		s = max(self.motors.values())
+		self.set_all_to(s)
+		for w in range(s, 1, -1):
+			mymotor.__W = w
+			mymotor.setW(w)
+			time.sleep(0.5)
 
 
-	def up(self):
