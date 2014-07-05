a4c6df644f48d1e14738ac0c1933ba05b15fb058
MROS <yc1043@gmail.com>
2014-07-05 06:25:11 +0000

    descend control by 0.5/1sec

---
+++ controller.py
@@ -0,0 +1,13 @@
+from motor import motor
+
+class quadcopter(objact):
+	def __init__(self):
+		self.motors = 
+		{'west' : motor('west', 17, sumulation=False),
+		 'east' : motor('east', 21, sumulation=False),
+		 'south' : motor('south', 23, sumulation=False),
+		 'north' : motor('north', 24, sumulation=False) }
+	def start(self):
+
+	def up(self):
+		
